4822dc4f12fdcaa8a4101f9d3c2211213d74c63e
ogelbw <ogelbw@gmail.com>
2023-08-16 00:28:51 +0100

    even more usable readout

--- auto_pilot.lua
+++ auto_pilot.lua
@@ -2,6 +2,11 @@ helm = peripheral.wrap("right")
 ship_reader = peripheral.wrap("bottom")
 
 print("Github script loaded")
-print("x" .. ship_reader.getWorldspacePosition().x)
-print("y" .. ship_reader.getWorldspacePosition().y)
-print("z" .. ship_reader.getWorldspacePosition().z)
+
+while true do
+    term.clear()
+    print("x " .. ship_reader.getWorldspacePosition().x)
+    print("y " .. ship_reader.getWorldspacePosition().y)
+    print("z " .. ship_reader.getWorldspacePosition().z)
+    sleep(0.5)
+end
