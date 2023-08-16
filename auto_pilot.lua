2b02d695dcfe5d1d554ce3129b0a6b3c2ac54c4e
ogelbw <ogelbw@gmail.com>
2023-08-16 22:51:02 +0100

    testing_auto_pilot

--- auto_pilot.lua
+++ auto_pilot.lua
@@ -1,12 +1,46 @@
 helm = peripheral.wrap("right")
 ship_reader = peripheral.wrap("bottom")
+args = {...}
 
-print("Github script loaded")
+function split (inputstr, sep)
+    if sep == nil then
+            sep = "%s"
+    end
+    local t={}
+    for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+            table.insert(t, str)
+    end
+    return t
+end
+
+
+local f = fs.open(args[1]..".coords")
+target = fs.readAll()
+f.close()
+target = split(target, ",")
+target = {
+    ['x']   = target[1],
+    ['y']   = target[2],
+    ['z']   = target[3],
+    ['yaw'] = target[4]
+}
+
+rot_error = math.atan2(target.z, target.pos.x)
 
 while true do
-    term.clear()
-    print("x " .. ship_reader.getWorldspacePosition().x)
-    print("y " .. ship_reader.getWorldspacePosition().y)
-    print("z " .. ship_reader.getWorldspacePosition().z)
-    sleep(0.5)
+    local pos = ship_reader.getWorldspacePosition()
+    local error = {
+        ['x']   = target.x - pos.x,
+        ['y']   = target.y - pos.y,
+        ['z']   = target.z - pos.z,
+        ['yaw'] = math.atan2(target.z - pos.z, target.x - pos.x)
+    }
+
+    if error.yaw < 0 then
+        helm.move('left', true)
+        helm.move('right', false)
+    else
+        helm.move('left', false)
+        helm.move('right', true)
+    end
 end
